962636b37f91bc761eb0843538096e1ab314ad16
Sahil-kangle-01 <sahil.jain12r@gmail.com>
2026-04-05 18:50:14 +0530

    Changes in Cors main.py

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ app = FastAPI(title="MediLoop API")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
 )
